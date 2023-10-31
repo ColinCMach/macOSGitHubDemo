2ff63db5e0bffc57af9023dca0f682dfa318d415
Josh Johnson <joshj@MacBook-Air-190.local>
2023-10-31 14:12:35 -0400

    Please commit

--- main.py
+++ main.py
@@ -6,13 +6,34 @@ def encode(password):
 
     return new_password
 
+def decode(password):
+    new_password = ""
+    for i in password_input:
+        new_password += str(int(i) - 3)
+
+    return new_password
 
 if __name__ == '__main__':
-    password_input = input("Enter your password: ")
+    password = ""
+    og_pass = ""
+    go = True
+    while go:
+        print("Menu\n-----------\n1. Encode\n2. Deocde\n3. Quit")
+        choice = input("Please enter an Option")
 
-    new_password = encode(password_input)
+        if choice == "1":
+            password_input = input("Enter your password: ")
+            og_pass = password_input
+            new_password = encode(password_input)
+            print("Encoded Password is ", new_password)
+            print("Your password has been encoded and stored!")
+            password = new_password
 
-    print("Encoded Password is ", new_password)
+        if choice == "2":
+            new_password = encode(password)
+            print(f"The encoded password is {new_password}, and the original password is {og_pass}")
+
+
 
 
 
